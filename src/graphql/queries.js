67b4d640567dc615df9cbacbfff22e50c3bd3742
David Poindexter <drpoindexter@gmail.com>
2019-09-05 12:59:50 -0400

    Add graphql api backend

---
+++ src/graphql/queries.js
@@ -0,0 +1,30 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const getPost = `query GetPost($id: ID!) {
+  getPost(id: $id) {
+    id
+    clientId
+    markdown
+    title
+    createdAt
+  }
+}
+`;
+export const listPosts = `query ListPosts(
+  $filter: ModelPostFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listPosts(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      clientId
+      markdown
+      title
+      createdAt
+    }
+    nextToken
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreatePost = `subscription OnCreatePost {
+  onCreatePost {
+    id
+    clientId
+    markdown
+    title
+    createdAt
+  }
+}
+`;
+export const onUpdatePost = `subscription OnUpdatePost {
+  onUpdatePost {
+    id
+    clientId
+    markdown
+    title
+    createdAt
+  }
+}
+`;
+export const onDeletePost = `subscription OnDeletePost {
+  onDeletePost {
+    id
+    clientId
+    markdown
+    title
+    createdAt
+  }
+}
+`;
